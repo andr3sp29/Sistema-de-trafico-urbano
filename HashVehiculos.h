2f7305bedeed082ff798b1db8ea96cd91f8afc81
Ninfakamy <197530383+Ninfakamy@users.noreply.github.com>
2025-12-02 19:58:08 -0600

    Update HashVehiculos.h

--- HashVehiculos.h
+++ HashVehiculos.h
@@ -1,1 +1,44 @@
+#pragma once 
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+struct Vehiculo{
+string id;
+string tipo;
+string placa;
+int origen;
+int destino;
+string hora;
+
+Vehiculo() : origen(-1), destino(-1){}
+
+};
+
+class HashVehiculos{
+public:
+explicit HashVehiculos(size_t mIni =101);
+
+bool insertar (const Vehiculo &v);
+const Vehiculo* buscar(const string &id) const;
+bool eliminar(const string &id);
+
+bool guardarCSV(const string &ruta) const;
+bool cargarCSV(const string &ruta);
+
+void info(ostream &o = cout) const;
+
+ size_t size() const { return nElems; }
+size_t buckets() const { return m; }
+
+private:
+ vector<vector<Vehiculo>> tabla;
+size_t m;
+size_t nElems;
+
+ size_t hashStr(const string &s) const;
+   void rehash(size_t newM);
+};
 
